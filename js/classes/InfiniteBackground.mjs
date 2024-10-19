21731545df22ad1d2e645110d4178f721389494e
Shahrin Shahrulzaman <shahrin14@gmail.com>
2024-10-20 01:55:08 +0900

    Infinite background

---
+++ js/classes/InfiniteBackground.mjs
@@ -0,0 +1,33 @@
+import { Sprite } from "https://cdn.jsdelivr.net/npm/pixi.js@8.x/dist/pixi.min.mjs";
+
+export class InfiniteBackground {
+    constructor(app) {
+
+        this.app = app
+        const spriteAliases = Array.from({ length: 5 }, (_, i) => `background${i + 1}`);
+        const spriteRotation = [0, 0.5, 1, 1.5, 0]
+
+        this.sprites = spriteAliases.map((spriteAlias, index) => {
+            const sprite = Sprite.from(spriteAlias);
+            sprite.x = 0.5 * app.screen.width;
+            sprite.y = 0.5 * app.screen.height - (sprite.height * index);
+            sprite.anchor.set(0.5);
+            sprite.rotation = Math.PI * spriteRotation[index]
+            app.stage.addChild(sprite);
+            return sprite;
+        });
+    }
+    tick() {
+        const scrollSpeed = 0.5;
+        this.sprites.forEach(sprite => {
+            sprite.y += scrollSpeed;
+        });
+
+        // Last background reached. Reset Y coordinates.
+        if (this.sprites[this.sprites.length - 1].y > (0.5 * this.app.screen.height)) {
+            this.sprites.forEach((sprite, index) => {
+                sprite.y = 0.5 * this.app.screen.height - (sprite.height * index);
+            })
+        }
+    }
+}
--- js/game.mjs
+++ js/game.mjs
@@ -1,6 +1,7 @@
 import { Assets } from "https://cdn.jsdelivr.net/npm/pixi.js@8.x/dist/pixi.min.mjs";
 
 import { GameApplication } from "./classes/GameApplication.mjs";
+import { InfiniteBackground } from "./classes/InfiniteBackground.mjs";
 import { Player } from "./classes/Player.mjs";
 
 let app = new GameApplication();
@@ -8,10 +9,12 @@ let app = new GameApplication();
 (async () => {
   await setup();
   await preload();
+  const background = new InfiniteBackground(app)
   const player = new Player(app);
 
   app.setup();
   app.ticker.add((time) => {
+    background.tick(time.deltaTime);
     player.tick(time.deltaTime);
   });
 })();
@@ -26,6 +29,21 @@ async function preload() {
     {
       alias: "spaceship",
       src: "assets/ship/spaceShips_001.png",
+    }, {
+      alias: "background1",
+      src: "assets/backgrounds/corona_ft.png",
+    }, {
+      alias: "background2",
+      src: "assets/backgrounds/corona_up.png",
+    }, {
+      alias: "background3",
+      src: "assets/backgrounds/corona_bk.png",
+    }, {
+      alias: "background4",
+      src: "assets/backgrounds/corona_dn.png",
+    }, {
+      alias: "background5",
+      src: "assets/backgrounds/corona_ft.png",
     },
   ];
   await Assets.load(assets);
